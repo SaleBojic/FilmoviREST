e6c6d3084ab8d63baeb928f43bcea69d5edbd2e6
SaleBojic <sale.bo1999@gmail.com>
2022-04-22 16:53:55 +0200

    starter project with .gitignore

---
+++ FilmoviRESTAPI/src/main/java/com/filmovi/sboot/FilmoviRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.filmovi.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmoviRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmoviRestapiApplication.class, args);
+	}
+
+}
